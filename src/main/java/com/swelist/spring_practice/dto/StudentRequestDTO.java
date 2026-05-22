91b23f298eb616f333db3e1f66ab6f996321d6c6
Kene Ijeh <keneijeh760@gmail.com>
2026-05-22 14:35:50 +0100

    requestdto

---
+++ src/main/java/com/swelist/spring_practice/dto/StudentRequestDTO.java
@@ -0,0 +1,27 @@
+package com.swelist.spring_practice.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentRequestDTO {
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NotNull
+    private Integer level;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String department;
+
+
+}

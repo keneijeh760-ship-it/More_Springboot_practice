0c6dd9a02fb96ec0b890820a7920223e2eff2ff1
Kene Ijeh <keneijeh760@gmail.com>
2026-05-24 00:12:25 +0100

    user service

--- src/main/java/com/swelist/spring_practice/Service/UserService.java
+++ src/main/java/com/swelist/spring_practice/Service/UserService.java
@@ -1,9 +1,44 @@
 package com.swelist.spring_practice.Service;
 
+import com.swelist.spring_practice.Repository.UserRepository;
+import com.swelist.spring_practice.dto.RegisterRequest;
+import com.swelist.spring_practice.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User createUser(RegisterRequest registerRequest) {
+
+         User user = mapToUser(registerRequest);
+         Optional<User> optionalUser = userRepository.findById(user.getId());
+
+         if (optionalUser.isPresent()) {
+             throw new RuntimeException("User with id " + user.getId() + " already exists");
+         }
+         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+         user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+         return userRepository.save(user);
+
+
+    }
+
+    private User mapToUser(RegisterRequest registerRequest) {
+        return User.builder()
+                .id(registerRequest.getId())
+                .firstName(registerRequest.getFirstName())
+                .lastName(registerRequest.getLastName())
+                .email(registerRequest.getEmail())
+                .password(registerRequest.getPassword())
+                .build();
+
+
+    }
 }
--- src/main/java/com/swelist/spring_practice/entity/User.java
+++ src/main/java/com/swelist/spring_practice/entity/User.java
@@ -1,15 +1,22 @@
 package com.swelist.spring_practice.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
-
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
